986ad69c7470106d764ab7d914c036ae6168bae1
fisheeesh <swanphyo444@gmail.com>
2025-10-07 23:32:48 +0700

    chore: use dictionary as sample db to test endpoints

--- app/main.py
+++ app/main.py
@@ -4,15 +4,58 @@ from typing import Any
 
 app = FastAPI()
 
+shipments = {
+    162002: {
+        "weight": 0.6,
+        "content": "glassware",
+        "status": "placed",
+    },
+    162003: {
+        "weight": 2.3,
+        "content": "electronics",
+        "status": "shipped",
+    },
+    162004: {
+        "weight": 1.5,
+        "content": "books",
+        "status": "delivered",
+    },
+    162005: {
+        "weight": 0.8,
+        "content": "clothing",
+        "status": "processing",
+    },
+    162006: {
+        "weight": 3.2,
+        "content": "furniture",
+        "status": "placed",
+    },
+    162007: {
+        "weight": 0.4,
+        "content": "cosmetics",
+        "status": "shipped",
+    },
+    162008: {
+        "weight": 5.7,
+        "content": "sports equipment",
+        "status": "cancelled",
+    },
+}
+
 
 @app.get("/shipment/latest")
-def get_latest_shipment():
-    return {"id": "12349392", "weight": 0.6, "content": "glassware", "status": "placed"}
+def get_latest_shipment() -> dict[str, Any]:
+    id = max(shipments.keys())
+
+    return shipments[id]
 
 
 @app.get("/shipment/{id}")
 def get_shipment(id: int) -> dict[str, Any]:
-    return {"id": id, "weight": 1.2, "content": "wooden table", "status": "in transit"}
+    if id not in shipments:
+        return {"details": "Given id doesn't exit!"}
+
+    return shipments[id]
 
 
 @app.get("/scalar", include_in_schema=False)
