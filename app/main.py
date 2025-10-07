78fbb3927c42583e64a46ac72161c1afa0b6a2c9
fisheeesh <swanphyo444@gmail.com>
2025-10-07 23:19:20 +0700

    chore: learned how to receive path parameter in fastapi

--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
-from fastapi import FastAPI # type: ignore
-from scalar_fastapi import get_scalar_api_reference # type: ignore
+from fastapi import FastAPI  # type: ignore
+from scalar_fastapi import get_scalar_api_reference  # type: ignore
+from typing import Any
 
 app = FastAPI()
 
-@app.get("/shipment")
-def get_shipment():
-    return {
-        "content" : "wooden table",
-        "status": "in transit"
-    }
-    
+
+@app.get("/shipment/{id}")
+def get_shipment(id: int) -> dict[str, Any]:
+    return {"id": id, "weight": 1.2, "content": "wooden table", "status": "in transit"}
+
+
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():
-    return get_scalar_api_reference(
-        openapi_url=app.openapi_url,
-        title="Scalar API"
-    )
+    return get_scalar_api_reference(openapi_url=app.openapi_url, title="Scalar API")
