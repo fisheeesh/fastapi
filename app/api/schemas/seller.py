ead2fdf922b7931867379fcc04bd70cf31242dfc
fisheeesh <swanphyo444@gmail.com>
2025-10-09 02:30:14 +0700

    chore: stored hash password with the help of bcrypt

--- app/api/schemas/seller.py
+++ app/api/schemas/seller.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel, EmailStr  # type: ignore
 
 
-class SellerCreate(BaseModel):
+class BaseSeller(BaseModel):
     name: str
     email: EmailStr
+
+
+class SellerRead(BaseSeller):
+    pass
+
+
+class SellerCreate(BaseSeller):
     password: str
